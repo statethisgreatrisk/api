9660f27dbd6c598ac3b7b3f1da05c34e7cea4ecd
Zachary <zacharycoss@gmail.com>
2024-09-13 05:27:56 -0500

    adding api-endpoint-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiListComponent } from './components/api-list/api-list.component';
+import { ApiEndpointListComponent } from './components/api-endpoint-list/api-endpoint-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ApiListComponent],
+  imports: [RouterOutlet, ApiListComponent, ApiEndpointListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/api-endpoint-list/api-endpoint-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-endpoint-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './api-endpoint-list.component.html',
+  styleUrl: './api-endpoint-list.component.scss'
+})
+export class ApiEndpointListComponent {
+
+}
