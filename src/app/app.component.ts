7bcc16dfe26cba631cc9eb08a25c2398b1780e64
Zachary <zacharycoss@gmail.com>
2024-09-13 05:14:31 -0500

    adding api-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiListComponent } from './components/api-list/api-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ApiListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/api-list/api-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './api-list.component.html',
+  styleUrl: './api-list.component.scss'
+})
+export class ApiListComponent {
+
+}
