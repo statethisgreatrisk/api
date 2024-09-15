13b04ff68fc72ab07385f4f12f8a0205915713fe
Zachary <zacharycoss@gmail.com>
2024-09-15 00:45:29 -0500

    adding schema type dropdown to edit-schema

--- src/app/components/edit-schema/edit-schema.component.ts
+++ src/app/components/edit-schema/edit-schema.component.ts
@@ -1,12 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edit-schema',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './edit-schema.component.html',
   styleUrl: './edit-schema.component.scss'
 })
 export class EditSchemaComponent {
+  prefixDropdownVisible = false;
 
+  togglePrefixDropdown() {
+    this.prefixDropdownVisible = !this.prefixDropdownVisible;
+  }
 }
