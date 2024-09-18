fa5ed61887918e51223804325a0df147081ea579
Zachary <zacharycoss@gmail.com>
2024-09-18 00:07:06 -0500

    adding select dropdown to api-view

--- src/app/components/api-view/api-view.component.ts
+++ src/app/components/api-view/api-view.component.ts
@@ -1,12 +1,24 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-api-view',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgClass],
   templateUrl: './api-view.component.html',
   styleUrl: './api-view.component.scss'
 })
 export class ApiViewComponent {
+  dropdownVisible = false;
+  dropdownVisible2 = false;
 
+  toggleDropdown() {
+    if (this.dropdownVisible2) this.dropdownVisible2 = false;
+    this.dropdownVisible = !this.dropdownVisible;
+  }
+
+  toggleDropdown2() {
+    if (this.dropdownVisible) this.dropdownVisible = false;
+    this.dropdownVisible2 = !this.dropdownVisible2;
+  }
 }
