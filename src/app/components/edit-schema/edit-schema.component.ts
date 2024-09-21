ca3df468e6dabf63d6060bbb3c0a911303f5a2f5
Zachary <zacharycoss@gmail.com>
2024-09-21 09:12:23 -0500

    implementing update schema functionality

--- src/app/components/edit-schema/edit-schema.component.ts
+++ src/app/components/edit-schema/edit-schema.component.ts
@@ -1,17 +1,66 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppStateInit, Schema, User, View } from '../../store/interfaces/app.interface';
+import { selectSchemas, selectUser, selectView } from '../../store/selectors/app.selector';
+import { Store } from '@ngrx/store';
+import { Subscription, combineLatest } from 'rxjs';
+import { deselectService, updateSchema } from '../../store/actions/app.action';
 
 @Component({
   selector: 'app-edit-schema',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, FormsModule],
   templateUrl: './edit-schema.component.html',
   styleUrl: './edit-schema.component.scss'
 })
 export class EditSchemaComponent {
-  prefixDropdownVisible = false;
+  user: User | null = null;
+  view: View = { service: '', serviceId: '', window: '', windowId: '' };
+  schema: Schema | null = null;
+
+  sub: Subscription | null = null;
+
+  prefixDropdown = false;
+
+  constructor(
+    private store: Store<AppStateInit>,
+  ) {}
+
+  ngOnInit() {
+    this.initLatest();
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
+
+  initLatest() {
+    this.sub = combineLatest([
+      this.store.select(selectUser),
+      this.store.select(selectView),
+      this.store.select(selectSchemas),
+    ]).subscribe(([user, view, schemas]) => {
+      this.user = user;
+      this.view = view;
+
+      if (this.user && this.view && this.view.serviceId) {
+        const schema = schemas.find((existingSchema) => existingSchema._id === this.view.serviceId);
+        this.schema = schema ? { ...schema } : null;
+      }
+    });
+  }
+
+  cancel() {
+    this.store.dispatch(deselectService({ serviceName: this.view.service, serviceId: this.view.serviceId }));
+  }
+
+  save() {
+    if (!this.schema) return;
+    this.store.dispatch(updateSchema({ schema: this.schema }));
+  }
 
   togglePrefixDropdown() {
-    this.prefixDropdownVisible = !this.prefixDropdownVisible;
+    this.prefixDropdown = !this.prefixDropdown;
   }
 }
