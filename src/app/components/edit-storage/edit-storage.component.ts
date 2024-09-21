1d49fae95841dd64ac2991c949f361c4e6c05ef1
Zachary <zacharycoss@gmail.com>
2024-09-21 09:05:20 -0500

    implementing update storage functionality

--- src/app/components/edit-storage/edit-storage.component.ts
+++ src/app/components/edit-storage/edit-storage.component.ts
@@ -1,25 +1,72 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AppStateInit } from '../../store/interfaces/app.interface';
-import { selectWindow } from '../../store/actions/app.action';
+import { AppStateInit, User, View, Storage } from '../../store/interfaces/app.interface';
+import { deselectService, selectWindow, updateStorage } from '../../store/actions/app.action';
+import { combineLatest, Subscription } from 'rxjs';
+import { selectUser, selectView, selectStorages } from '../../store/selectors/app.selector';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-storage',
   standalone: true,
-  imports: [NgIf, NgClass],
+  imports: [NgIf, NgClass, FormsModule],
   templateUrl: './edit-storage.component.html',
   styleUrl: './edit-storage.component.scss'
 })
 export class EditStorageComponent {
-  dropdownVisible = false;
+  user: User | null = null;
+  view: View = { service: '', serviceId: '', window: '', windowId: '' };
+  storage: Storage | null = null;
+
+  sub: Subscription | null = null;
+
+  dropdown = false;
 
   constructor(
     private store: Store<AppStateInit>,
   ) {}
 
+  ngOnInit() {
+    this.initLatest();
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
+
+  initLatest() {
+    this.sub = combineLatest([
+      this.store.select(selectUser),
+      this.store.select(selectView),
+      this.store.select(selectStorages),
+    ]).subscribe(([user, view, storages]) => {
+      this.user = user;
+      this.view = view;
+
+      if (this.user && this.view && this.view.serviceId) {
+        const storage = storages.find((existingStorage) => existingStorage._id === this.view.serviceId);
+        this.storage = storage ? { ...storage } : null;
+      }
+    });
+  }
+
+  selectSchemaId(schemaId: string) {
+    if (!this.storage) return;
+    this.storage.schemaId = schemaId;
+  }
+
+  cancel() {
+    this.store.dispatch(deselectService({ serviceName: this.view.service, serviceId: this.view.serviceId }));
+  }
+
+  save() {
+    if (!this.storage) return;
+    this.store.dispatch(updateStorage({ storage: this.storage }));
+  }
+
   toggleDropdown() {
-    this.dropdownVisible = !this.dropdownVisible;
+    this.dropdown = !this.dropdown;
   }
 
   openView() {
