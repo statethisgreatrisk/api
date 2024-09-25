970587cb9a0ce4bacde59e1cc4b8e9b04d1a93fe
Zachary <zacharycoss@gmail.com>
2024-09-25 01:07:19 -0500

    drafting key-view

---
+++ src/app/components/key-view/key-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './key-view.component.html',
+  styleUrl: './key-view.component.scss'
+})
+export class KeyViewComponent {
+
+}
--- src/app/components/settings-view/settings-view.component.ts
+++ src/app/components/settings-view/settings-view.component.ts
@@ -4,6 +4,7 @@ import { SettingsService } from '../../services/settings.service';
 import { AccountViewComponent } from '../account-view/account-view.component';
 import { DeployViewComponent } from '../deploy-view/deploy-view.component';
 import { LogViewComponent } from '../log-view/log-view.component';
+import { KeyViewComponent } from '../key-view/key-view.component';
 
 @Component({
   selector: 'app-settings-view',
@@ -14,12 +15,13 @@ import { LogViewComponent } from '../log-view/log-view.component';
     AccountViewComponent,
     DeployViewComponent,
     LogViewComponent,
+    KeyViewComponent,
   ],
   templateUrl: './settings-view.component.html',
   styleUrl: './settings-view.component.scss'
 })
 export class SettingsViewComponent {
-  setting: string = 'logs';
+  setting: string = 'apiKeys';
 
   constructor(private settingsService: SettingsService) {}
 
