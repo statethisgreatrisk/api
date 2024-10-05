bfd200e908b5f9229a03069e23da27ddd6a96d34
Zachary <zacharycoss@gmail.com>
2024-10-05 10:52:54 -0500

    updating fn and obj defaults

--- src/app/components/edit-fn/edit-fn.component.ts
+++ src/app/components/edit-fn/edit-fn.component.ts
@@ -72,7 +72,7 @@ export class EditFnComponent {
   createEditor() {
     let codeEditorElement = this.codeEditor.nativeElement;
     let myExt: Extension = [basicSetup, javascript(), oneDarkSmall];
-    const fn = this.fn?.fn ? this.fn.fn : 'function fn(context, variable, args) {\n  \n}';
+    const fn = this.fn?.fn ? this.fn.fn : 'function fn(context) {\n  \n}';
     
     try {
       this.editorState = EditorState.create({
